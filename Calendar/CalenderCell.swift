6274561b81b51621212ed538432f0a93056d6edf
Zehra Nur Acik <zehranuracik@hotmail.com>
2024-07-31 21:27:33 +0300

    Calendar

---
+++ Calendar/CalenderCell.swift
@@ -0,0 +1,12 @@
+//
+//  CalenderCell.swift
+//  Calendar
+//
+//  Created by Zehra Nur Açık on 3.12.2023.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell {
+    @IBOutlet weak var dayOfMonth: UILabel!
+}
